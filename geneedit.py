1f6ecf0fd775ba15f18ee8389775ec02b5996731
通晓宇宙 <2581011320@qq.com>
2022-06-12 23:21:31 +0800

    基因编辑

---
+++ geneedit.py
@@ -0,0 +1,13 @@
+def geneedit(a, b):
+    dp = [[0 for i in range(len(b)+1)] for j in range(len(a)+1)]
+    for i in range(len(a)+1):
+        dp[i][0] = i
+    for j in range(len(b)+1):
+        dp[0][j] = j
+    for i in range(1, len(a)+1):
+        for j in range(1, len(b)+1):
+            if a[i-1] == b[j-1]:
+                dp[i][j] = dp[i-1][j-1]
+            else:
+                dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
+    return dp[len(a)][len(b)]
