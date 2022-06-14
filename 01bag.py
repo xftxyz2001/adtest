de6a0a7c22869d9ead9347fe60e4b9f73b0df627
通晓宇宙 <2581011320@qq.com>
2022-06-14 13:38:40 +0800

    不大于w的情况

--- 01bag.py
+++ 01bag.py
@@ -2,6 +2,14 @@ def bag(wv, W):
     n_maxv = [len(wv), 0]
     x = [-1]*n_maxv[0]
 
+    def bound(i, tw, tv):
+        i += 1
+        while i < n_maxv[0] and tw + wv[i][0] <= W:
+            tw += wv[i][0]
+            tv += wv[i][1]
+            i += 1
+        return tv + (W - tw) * (wv[i][1]/wv[i][0]) if i < n_maxv[0] else tv
+
     def dfs(i, tw, tv, op):
         if i >= n_maxv[0]:
             if tw == W and tv > n_maxv[1]:
@@ -11,7 +19,7 @@ def bag(wv, W):
         if tw + wv[i][0] <= W:
             op[i] = 1
             dfs(i+1, tw+wv[i][0], tv+wv[i][1], op)
-        if tw + sum([wv[_][0] for _ in range(i+1, n_maxv[0])]) >= W:
+        if bound(i, tw, tv) > n_maxv[1]:
             op[i] = 0
             dfs(i+1, tw, tv, op)
     dfs(0, 0, 0, [-1]*n_maxv[0])
