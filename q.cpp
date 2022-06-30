9e73c4167c48b403ef768dde768654f5bdc83069
通晓宇宙 <2581011320@qq.com>
2022-06-30 20:28:10 +0800

    ...

--- q.cpp
+++ q.cpp
@@ -12,10 +12,10 @@ class Queen
     friend void nQueen(int);
 
 private:
-    bool Place(int k);   //测试皇后k置于第x[k]列的合法性
-    bool QueensLV(void); //随机放置n个皇后拉斯维加斯算法
-    int n;               //皇后个数
-    int *x;              //解向量
+    bool Place(int k);            //测试皇后k置于第x[k]列的合法性
+    void Backtrack(int t);        //★解n后问题的回溯法
+    bool QueensLV(int stopVegas); //★随机放置n个皇后的拉斯维加斯算法
+    int n, *x, *y;
 };
 bool Queen::Place(int k)
 { //测试皇后k置于第x[k]列的合法性
@@ -24,11 +24,28 @@ bool Queen::Place(int k)
             return false;
     return true;
 }
-bool Queen::QueensLV(void)
+void Queen::Backtrack(int t)
+{
+    if (t > n) // t来记当前已经放置的皇后数
+    {
+        for (int i = 1; i <= n; i++)
+            y[i] = x[i];
+        return;
+    } //已找到一个解并复制给y.
+    else
+    {
+        for (int i = 1; i <= n; i++)
+            x[t] = i;
+        if (Place(t))
+            Backtrack(t + 1);
+    }
+}
+bool Queen::QueensLV(int stopVegas)
 { //随机放置n个皇后的拉斯维加斯算法
     int k = 1;
     int count = 1;
-    while ((k <= n) && (count > 0))
+    // 1≤stopVegas≤n表示允许随机放置的皇后数
+    while ((k <= stopVegas) && (count > 0))
     {
         count = 0;
         int j = 0;
@@ -45,20 +62,27 @@ bool Queen::QueensLV(void)
     return (count > 0); // count>0表示成功放置
 }
 void nQueen(int n)
-{
+{ //与回溯法相结合的解n后问题的拉斯维加斯算法
     Queen x;
-    //初始化x
     x.n = n;
+    int *q = new int[n + 1];
     int *p = new int[n + 1];
     for (int i = 0; i <= n; i++)
+    {
         p[i] = 0;
-    x.x = p;
-    //反复调用随机放置n个皇后的拉斯维加斯算法，直至放置成功
-    while (!x.QueensLV())
+        q[i] = 0;
+    }
+    x.x = q;
+    x.y = p;
+    int stop = 5;
+    while (!x.QueensLV(stop))
         ;
-    for (int i = 1; i <= n; i++)
+    x.Backtrack(stop + 1);
+    for (int i = 1; i <= 5; i++)
         cout << p[i] << " ";
+    cout << endl;
     delete[] p;
+    delete[] q;
 }
 
 int main(int argc, char const *argv[])
